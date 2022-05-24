65dd4043a6c06db004dcbb958e5bb5602ada7414
Michael <106043569+MichaelQuimbayo@users.noreply.github.com>
2022-05-23 23:57:46 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Home from './pages/home/home';
+import './App.scss';
+
+function App() {
+  return <div className='App'>
+    <Home />
+  </div>
+
+}
+
+export default App;
---
+++ src/pages/home/home.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Button from '@mui/material/Button'
+
+
+//import { Fragment } from "react"
+
+function Home(){
+    return(
+        
+           <div> 
+               <Button variant="contained">Contained</Button>
+               
+               </div>
+        
+        
+    );
+}
+
+export default Home;
